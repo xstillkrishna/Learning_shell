9c9f26ec04801189955e477f2fdcce7136bbe457
xstillkrishna <freefire1894560@gmail.com>
2026-01-13 18:25:03 +0530

    New file of user input's arguement

---
+++ userInput.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Syntax
+
+read variable  #variable stores data 
+
+#OR
+
+read -n 5 -t 10 -sp "Enter your thougth: " -a array
+
+:<< EOF 
+-n for number words,
+-t for times in sec,
+-s for silent for password,
+-p for writing paragraph,
+-a for array
+EOF
