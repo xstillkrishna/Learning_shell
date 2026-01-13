fc5d3e8a33eaa566392c6beb4e992ab342d78c4b
xstillkrishna <freefire1894560@gmail.com>
2026-01-14 02:24:39 +0530

    added comment

--- variables1.sh
+++ variables1.sh
@@ -7,13 +7,13 @@ echo "my name is $name and I'm $age yr old"
 
 name=Anonymous
 
-echo "New name is ${name}"
+echo "New name is ${name}"  #safer way to echo with qoute.
 
-HOSTNAME=$(hostname)
+HOSTNAME=$(hostname)  #HOSTNAME stores the value of hostname after it run
 
 echo "The name of the present server is $HOSTNAME"
 
-PWD=$(pwd)
+PWD=$(pwd)  #same PWD stores the value of pwd, means pwd first excutes and then result of pwd stored in PWD
 
 echo "We're in Path $PWD"
 
