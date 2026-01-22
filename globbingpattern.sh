b0fc7f8610fe988b6beaa4c5177f9c4b5ab6c44d
xstillkrishna <freefire1894560@gmail.com>
2026-01-22 12:51:59 +0530

    new file named globbingpattern

---
+++ globbingpattern.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#wildcard characters
+#---> *,? and [].
+
+#wildcard character + rules = globbing Pattern
+#alternative is regex known for like emails,phone validation etc.
+
+
+#1. start(*) 
+# matches all character except slash(/)
+#
