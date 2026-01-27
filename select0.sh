20f7ad01e012e26654e2f8c8b98f9958b536fa9c
xstillkrishna <freefire1894560@gmail.com>
2026-01-27 20:20:46 +0530

    first time added select documentation

---
+++ select0.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#select syntax
+
+while true
+do
+    select choice in Start Stop Exit
+    do
+        case $choice in
+            Start)
+                echo "Service started"
+                break
+                ;;
+            Stop)
+                echo "Service stopped"
+                break
+                ;;
+            Exit)
+                echo "Bye!"
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice"
+                ;;
+        esac
+    done
+done
+
+#output---> 1)Start
+#           2)Stop
+#           3)Exit
+
